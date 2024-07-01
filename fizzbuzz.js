93bbee4fc72058f60828fd94e9167215ca5f447b
Anum Riaz <anum.riaz@softwire.com>
2024-07-01 15:33:50 +0100

    prints number from 1 to 100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,12 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz!');
+    //console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
-
-    
+    // Print all number from 1 TO 100
+    for (let i = 1; i < 101; i++) {
+        console.log(i);
+    }  
 }
 
 // Now we run the main function...
