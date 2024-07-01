4b5672b039d5ed783a53fa259031d49a9d8f457d
Anum Riaz <anum.riaz@softwire.com>
2024-07-01 15:55:12 +0100

    print fizz if the number is a multiple of 3 and buzz if multiple of 5 , fizzbuzz if multiple of 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,8 +7,12 @@ function fizzbuzz() {
 
     // Print all numbers from 1 to 100 , print fizz instead if the number is a multiple of 3
     for (let i = 1; i < 101; i++) {
-        if (i % 3 === 0){
+        if (i % 3 === 0  && i %5 == 0){
+            console.log("FizzBuzz");
+        } else if (i %3 == 0){
             console.log("Fizz");
+        } else if (i %5 == 0){
+            console.log("Buzz");
         } 
         else{
             console.log(i);
